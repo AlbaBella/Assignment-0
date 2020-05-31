c35d756f491092996d8ea95776a4359d9a4f2f8f
Alba <alba.bella@cix.csi.cuny.edu>
2020-05-31 10:45:28 -0400

    Question 1

--- 01-titleCaseEdit/titleCaseEdit.js
+++ 01-titleCaseEdit/titleCaseEdit.js
@@ -1,9 +1,14 @@
 function titleCaseEdit(title) {
   // Insert code here;
-  var myTitle;
-  myTitle= title[0].toUpperCase() +  
-  title.slice(1); 
-  return myTitle;
+
+  //seperate each word of the title 
+  var myTitle= title.split(' ');
+
+  for(var i=0;i<myTitle.length;i++){
+    myTitle[i]= myTitle[i].charAt(0).toUpperCase() + myTitle[i].substring(1);
+  }
+   //put wach word back with space and return the title
+  return myTitle.join(' ');
 }
 
 // Do not edit this line;
