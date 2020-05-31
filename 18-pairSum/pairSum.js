46206a04196fdb40e8d8ce86880adc7d7e199cb7
Alba <alba.bella@cix.csi.cuny.edu>
2020-05-31 16:55:00 -0400

    Question 18

--- 18-pairSum/pairSum.js
+++ 18-pairSum/pairSum.js
@@ -1,5 +1,17 @@
 function pairSum(nums, target) {
   // Insert code here;
+
+  if (nums.length<=1){
+    throw "Please enter an array of atleast 2 numbers";
+
+  }
+  for(var i=0;i<nums.length;i++){
+      for(var j=nums.length;j>i;j--){
+        if (nums[i]+nums[j]==target)
+        return true;
+      }
+    }
+  return false;
 }
 
 // Do not edit this line;
