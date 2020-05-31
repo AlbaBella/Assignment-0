0a0cba814564ac87018d76b9329b50d9bc64356b
Alba <alba.bella@cix.csi.cuny.edu>
2020-05-31 15:38:19 -0400

    Question 14

--- 14-frequencyCounter/frequencyCounter.js
+++ 14-frequencyCounter/frequencyCounter.js
@@ -1,5 +1,18 @@
 function frequencyCounter(word) {
   // Insert code here;
+
+  var freq={ };
+  for (var i=0; i<word.length;i++) {
+    var character = word.charAt(i);
+    //checking if freq[character] is false
+    
+    if (freq[character]) {
+       freq[character]++;
+    } else {
+       freq[character] = 1;
+    }
+}
+return freq;
 }
 
 // Do not edit this line;
