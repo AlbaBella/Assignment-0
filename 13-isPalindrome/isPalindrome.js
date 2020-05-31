46ba3e544b2eb93cd8dc8efbe06b5700a346189d
Alba <alba.bella@cix.csi.cuny.edu>
2020-05-31 15:20:23 -0400

    Question 13

--- 13-isPalindrome/isPalindrome.js
+++ 13-isPalindrome/isPalindrome.js
@@ -1,5 +1,22 @@
 function isPalindrome(word) {
   // Insert code here;
+
+  var mid=Math.floor(word.length/2); //finding the half length of the word
+  var isPalindrome=true;
+  var start=0; //starting point
+  var end=word.length-1;//ending point
+  
+  //comparing the letters from start to end
+  for(var i=0;i<mid;i++){
+    if (word[start] !== word[end]) {
+      isPalindrome=false;
+    }
+    start++;
+    end--;
+  }
+  
+  return isPalindrome;
+
 }
 
 // Do not edit this line;
