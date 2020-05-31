39d76a13a4e87bc012c74db276e14600c184b711
Alba <alba.bella@cix.csi.cuny.edu>
2020-05-31 16:30:46 -0400

    Question 16

--- 16-productOfAnyAmountOfNumbers/productOfAnyAmountOfNumbers.js
+++ 16-productOfAnyAmountOfNumbers/productOfAnyAmountOfNumbers.js
@@ -1,5 +1,9 @@
 function productOfAnyAmountOfNumbers(...args) {
   // Insert code here;
+  //var product=0;
+  return args.reduce((prev, curr) => {
+    return prev * curr;
+  });
 }
 
 // Do not edit this line;
