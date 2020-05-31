663fcb033a245e23cce7b8ca71c09cfb1d1247a9
Alba <alba.bella@cix.csi.cuny.edu>
2020-05-31 13:56:26 -0400

    Question 11

--- 11-sumOfMinimumAndMaximum/sumOfMinimumAndMaximum.js
+++ 11-sumOfMinimumAndMaximum/sumOfMinimumAndMaximum.js
@@ -1,5 +1,15 @@
 function sumOfMinimumAndMaximum(nums) {
   // Insert code here;
+  //set the first num as the min
+  const min= Math.min(...nums);
+  
+  //set the last num as the max
+  const max= Math.max(...nums);
+
+  var sum=min+max;
+
+  return sum;
+
 }
 
 // Do not edit this line;
