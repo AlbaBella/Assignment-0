1b693cfb01d8d647d0c5629f783f7fc4beb2252f
Alba <alba.bella@cix.csi.cuny.edu>
2020-05-31 14:52:13 -0400

    Question 12

--- 12-fizzBuzz/fizzBuzz.js
+++ 12-fizzBuzz/fizzBuzz.js
@@ -1,5 +1,26 @@
 function fizzBuzz(start, end) {
   // Insert code here;
+
+  //inizialing the array
+  var myArr=[];
+
+  for (var i=start;i<=end;i++){
+        
+    if (i % 3 ==0 && i % 5 == 0){
+      myArr.push("FizzBuzz");
+    }
+    else if (i % 5 == 0){
+      myArr.push("Buzz");
+    }
+    else if (i % 3 == 0){
+      myArr.push("Fizz");
+    }
+    else {
+      myArr.push(i);
+    }
+    
+  }
+  return myArr;
 }
 
 // Do not edit this line;
