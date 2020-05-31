1ab03a65e6a28d09f86567b0f6ef5e105ae8e83b
Alba <alba.bella@cix.csi.cuny.edu>
2020-05-31 16:41:57 -0400

    Question 17

--- 17-setUnionOfAnyAmountOfSets/setUnionOfAnyAmountOfSets.js
+++ 17-setUnionOfAnyAmountOfSets/setUnionOfAnyAmountOfSets.js
@@ -1,5 +1,13 @@
 function setUnionOfAnyAmountOfSets(...args) {
   // Insert code here;
+
+  var mySet= new Set();
+  for (let sets of args){
+    for (let num of sets){
+      mySet.add(num);
+    }
+  }
+    return mySet;
 }
 
 // Do not edit this line;
