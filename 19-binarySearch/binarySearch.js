c0cced0aecd012c0701edffedf18f33efd16f8a6
Alba <alba.bella@cix.csi.cuny.edu>
2020-05-31 17:13:11 -0400

    Question 19

--- 19-binarySearch/binarySearch.js
+++ 19-binarySearch/binarySearch.js
@@ -5,7 +5,23 @@ class MySolution {
 
   binarySearch(nums, target) {
     // Insert code here;
+
+    var found = false;
+    var mid = Math.ceil(nums.length / 2)-1;
+    var left = nums.slice(0, mid + 1);
+    let right = nums.slice(mid + 1, nums.length);
+
+    if (nums[mid] === target)
+      return true;
+    if(1 < nums.length ) {
+      if (nums[mid] > target)
+        found = this.binarySearch(left, target);
+      else
+        found = this.binarySearch(right, target);
+    }
+    return found;
   }
+ 
 }
 
 // Do not edit this line;
